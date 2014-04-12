611c667443ea0cb7b876ab1bd021d61d0a417af8
Kashev Dalmia <kashev.dalmia@gmail.com>
2014-04-12 11:26:35 -0700

    working countdown timer

--- pebble/nextWAVE/src/nextWAVE.c
+++ pebble/nextWAVE/src/nextWAVE.c
@@ -14,39 +14,95 @@
 #include <pebble.h>
 
 /*
- * Every Pebble App Has a Window & at least one Text Layer
+ * CONSTANTS
  */
-static Window *window;
-static TextLayer *text_layer;
+#define NEXTWAVE_FONT FONT_KEY_ROBOTO_BOLD_SUBSET_49
+#define FONT_HEIGHT   49
+#define TOPBAR_HEIGHT 20
+#define TIME_FORMAT "%M:%S"
 
 /*
- * CLICK HANDLING
+ * STATIC GLOBALS
+ */
+/* Every pebble app has a window and at least one text layer */
+static Window *window;
+static TextLayer *text_layer;
+/* Frame to create text layer size */
+static GRect timeFrame = {
+    .origin = {
+        .x = 0,
+        .y = ((168 + TOPBAR_HEIGHT)/2) - (FONT_HEIGHT)
+    },
+    .size = {
+        .w = 144,
+        .h = 168
+    }
+};
+/* Stored Time */
+static char time_text[] = "00:00";
+static uint32_t seconds_left = 10;
+
+// /*
+//  * CLICK HANDLING
+//  */
+// static void
+// select_click_handler (ClickRecognizerRef recognizer, void *context)
+// {
+//     text_layer_set_text(text_layer, "BradyRocks");
+// }
+
+// static void
+// up_click_handler (ClickRecognizerRef recognizer, void *context)
+// {
+//     text_layer_set_text(text_layer, "Up");
+// }
+
+// static void
+// down_click_handler (ClickRecognizerRef recognizer, void *context)
+// {
+//     text_layer_set_text(text_layer, "Down");
+// }
+
+// static void
+// click_config_provider (void *context)
+// {
+//     window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+//     window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+//     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+// }
+
+/*
+ * TIME RENDERING
  */
 static void
-select_click_handler (ClickRecognizerRef recognizer, void *context)
+render_time (uint32_t s)
 {
-    text_layer_set_text(text_layer, "Select");
+    struct tm t;
+    t.tm_min = s / 60;
+    t.tm_sec = s % 60;
+
+    strftime(time_text, sizeof(time_text), TIME_FORMAT, &t);
 }
 
+/*
+ * TIME HANDLING
+ */
 static void
-up_click_handler (ClickRecognizerRef recognizer, void *context)
+handle_second_tick (struct tm* tick_time, TimeUnits units_changed)
 {
-    text_layer_set_text(text_layer, "Up");
+    if (seconds_left != 0)
+    {
+        seconds_left--;
+    }
+    if (seconds_left == 0)
+    {
+        vibes_long_pulse();
+    }
+
+    render_time(seconds_left);
+    text_layer_set_text(text_layer, time_text);
 }
 
-static void
-down_click_handler (ClickRecognizerRef recognizer, void *context)
-{
-    text_layer_set_text(text_layer, "Down");
-}
-
-static void
-click_config_provider (void *context)
-{
-    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
 
 static void
 window_load (Window *window)
@@ -54,10 +110,14 @@ window_load (Window *window)
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
-    text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-    text_layer_set_text(text_layer, "nextWAVE");
+    text_layer = text_layer_create(timeFrame);
+    text_layer_set_font(text_layer, fonts_get_system_font(NEXTWAVE_FONT));
+    text_layer_set_text(text_layer, "00:00");
     text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+    // handle_second_tick(current_time, SECOND_UNIT);
+    tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
 }
 
 static void
